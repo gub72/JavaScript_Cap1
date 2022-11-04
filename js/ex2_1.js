4035f94a23e22735678e7f6532141fc1e9e37f98
gub72 <nevesgustavo.dev@gmail.com>
2022-11-04 12:56:16 -0300

    Segundo teste

--- js/ex2_1.js
+++ js/ex2_1.js
@@ -3,7 +3,7 @@ const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 //cria um "ouvinte" de evento, acionado com o botão submit for clicado 
 frm.addEventListener("submit", (e) => { 
-    const nome = frm.inNome.value //obtem o nome digitado no form
-    resp.innerText = `Ola ${nome}` //exibe a resposta do programa
+    const nome1 = frm.inNome.value //obtem o nome digitado no form
+    resp.innerText = `Ola ${nome1}` //exibe a resposta do programa
     e.preventDefault() //evita de que o submit"botao" atualize a pagina
 })
